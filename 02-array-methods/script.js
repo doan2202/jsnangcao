4b285ff1f0e39c113503231ab25a1a02889114b4
doan2202 <doannvph39305@fpt.edu.vn>
2024-01-09 15:38:16 +0700

    tuan2

--- 02-array-methods/script.js
+++ 02-array-methods/script.js
@@ -67,4 +67,53 @@ x = Object.entries(todos);
 
   console.log(x);
 
+// object-literals
+// Creating an object
+const person = {
+  name: 'John Doe',
+  age: 30,
+  isAdmin: true,
+  address: {
+    street: '123 Main st',
+    city: 'Boston',
+    state: 'MA',
+  },
+  hobbies: ['music', 'sports'],
+};
+// destructuring-naming
+const { name } = person
+console.log ({name})
+// Accessing object properties
+x = person.name; // Dot notation
+x = person['age']; // Bracket notation
+x = person.address.state;
+x = person.hobbies[0];
+
+// Updating properties
+person.name = 'Jane Doe';
+person['isAdmin'] = false;
+
+// Deleting properties
+delete person.age;
+
+// Create new properties
+person.hasChildren = true;
+
+// Add functions
+person.greet = function () {
+  console.log(`Hello, my name is ${this.name}`);
+};
+
+person.greet();
+
+// Keys with multiple words
+const person2 = {
+  'first name': 'Brad',
+  'last name': 'Traversy',
+};
+
+x = person2['first name'];
+
+console.log(x);
+
 
