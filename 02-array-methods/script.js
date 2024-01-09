e5a2d83bbf43a74b79d03bb733209cbaf45fcc26
doan2202 <doannvph39305@fpt.edu.vn>
2024-01-09 15:26:03 +0700

    tuan2

--- 02-array-methods/script.js
+++ 02-array-methods/script.js
@@ -1,3 +1,4 @@
+// 2.1 array basic
 let x;
 
 const arr = [28, 38, 44, 29, 109];
@@ -23,20 +24,47 @@ x = arr.includes(445);
 // indexOf() - Return the index of the first match
 x = arr.indexOf(28);
 
-// Return array as a string
-x = arr.toString();
-x = arr.join();
-
 // slice() returns selected elements in an array, as a new array. Slice takes in the index of the first element and the index of the last element to be included in the new array.
 x = arr.slice(1, 4);
 
 // splice() works like slice() except it takes the index of the first element and the number of elements after that as a second argument. it also mutates the original array where slice() does not
 x = arr.splice(1, 4);
 
-// Remove a single element/value - The following will mutate the original array by taking out the element with the index of 4. x will be equal to a new array with that plucked out value.
-x = arr.splice(4, 1);
 
-// Chaining methods - Some methods can be chained depending on the return value.
-x = arr.slice(1, 4).reverse().toString().charAt(0);
+// array spread
+const obj1 = { a: 1, b: 2 };
+const obj2 = { c: 3, d: 4 };
 
-console.log(x);
+// Spread operator
+const obj3 = { ...obj1, ...obj2 };
+// Same as using ...
+const obj4 = Object.assign({}, obj1, obj2);
+console.log(obj3);
+console.log(obj4);
+
+// Array of objects
+const todos = [
+    { id: 1, name: 'Buy Milk' },
+    { id: 2, name: 'Pickup kids from school' },
+    { id: 3, name: 'Take out trash' },
+  ];
+  
+  y = todos[0].name;
+
+  console.log(y);
+  // Get array of object keys
+// x = Object.keys(todos);
+
+// Get length of an object
+// x = Object.keys(todos).length;
+
+// Get array of object values
+// x = Object.values(todos);
+
+// Get array of object key/value pairs
+x = Object.entries(todos);
+
+
+  console.log(x);
+
+
